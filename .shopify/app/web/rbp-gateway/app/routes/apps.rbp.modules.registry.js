cb541c6f25faea281f9ebb482cd4e8c612ada829
Cwegeleben <Chris@proliterods.com>
2025-08-18 17:40:17 -0700

    chore: switch gateway to express dev server; set app_proxy to localhost:3000

--- .shopify/app/web/rbp-gateway/app/routes/apps.rbp.modules.registry.js
+++
@@ -1,6 +0,0 @@
-import { readFile } from "node:fs/promises";
-import { join } from "node:path";
-export async function loader() {
-  const raw = await readFile(join(process.cwd(), "src", "config", "registry.json"), "utf8");
-  return new Response(raw, { headers: { "content-type": "application/json" } });
-}
